903b7177ef23c437bd07d58af2565d410834c43a
asinaga117 <asinaga1994@gmail.com>
2022-04-15 10:43:11 +0000

    user edit画面の作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   
   devise_for :users
-  resources :books, only:[ :index, :show, :create, :edit ]
-  resources :users, only:[:index,:show, :edit]
+  resources :books, only:[ :index, :show, :create, :edit, :destroy, :update ]
+  resources :users, only:[:index,:show, :edit, :update]
   root to: "homes#top"
   get 'homes/about' => 'homes#about',as: 'about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
